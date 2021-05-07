70d06b642bf3f395eabd2a4337e1144219ff4316
freddymi1 <michelfreddy1992@gmail.com>
2021-05-07 09:47:04 +0300

    Cours typescript

---
+++ index.js
@@ -0,0 +1,8 @@
+//Variables
+var num1 = 24;
+console.log(typeof num1);
+// let num2: number;
+var numberOrString; // union
+numberOrString = 5;
+// numberOrString = "Salut"
+console.log(numberOrString);
---
+++ index.ts
@@ -0,0 +1,11 @@
+//Variables
+let  num1 = 24
+
+console.log(typeof num1)
+
+// let num2: number;
+
+let numberOrString: number | string // union
+numberOrString = 5
+// numberOrString = "Salut"
+console.log(numberOrString)
